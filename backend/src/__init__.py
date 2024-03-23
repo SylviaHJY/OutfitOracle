ed0aee7055f25abfa751d6a08857155c4a981a9a
Leona <huangsylvia822@gmail.com>
2024-03-23 05:24:27 -0400

    Implemented initial environment config for flask

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 # from dotenv import load_dotenv
 from .routers.bg_remover import bg_remover
+from .routers.save_outfit import save_outfit
+from firebase_admin import credentials, firestore, initialize_app
 
 # load_dotenv('src/.env')
 
@@ -10,8 +12,16 @@ def create_app():
     # CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
     # CORS(app)
     CORS(app, resources={r"/*": {"origins": "*"}})
+    # Initialize Firebase Admin SDK
+    cred = credentials.Certificate('src/wardrobe-wizard-105dd-firebase-adminsdk.json')
+    initialize_app(cred, {
+        'storageBucket': 'wardrobe-wizard-105dd.appspot.com'
+    })
+    
+    # db = firestore.client()
     
     app.register_blueprint(bg_remover)
+    app.register_blueprint(save_outfit)
 
     @app.route("/")
     def hello():
