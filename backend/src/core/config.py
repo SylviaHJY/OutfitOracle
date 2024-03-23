3e8d34d7718e56306bf4ebccd6c26b92ca464b78
Leona <huangsylvia822@gmail.com>
2024-03-23 05:13:05 -0400

    assign every category to mapping relationship

---
+++ backend/src/core/config.py
@@ -0,0 +1,34 @@
+CLOTHING_CATEGORIES = {
+    # Tops
+    'T-shirts': 'top',
+    'Longsleeves': 'top',
+    'Tank tops': 'top',
+    'Hoodies': 'top',
+    'Blouses': 'top',
+    'Blazers & Vests': 'top',
+    'Sweaters': 'top',
+    'Jackets': 'top',
+    'Coats': 'top',
+    'Overcoats': 'top',
+    'Dresses': 'top',  
+
+    # Bottoms
+    'Jeans': 'bottom',
+    'Pants': 'bottom',
+    'Agency pant': 'bottom',
+    'Shorts': 'bottom',
+    'Skirts': 'bottom',
+    
+    # Shoes
+    'Shoes': 'shoes',
+    'Boots': 'shoes',
+    'Leather shoes': 'shoes',
+    'Sandals': 'shoes',
+    'Sneakers': 'shoes',
+    'Heels': 'shoes',
+    
+    # Accessories (not classified as top or bottom)
+    'Hats': 'accessory',
+    'Bags': 'accessory',
+    'Accessories': 'accessory'
+}
