955988869b40c17f10387eaf317d64cf808b33e4
Leona <huangsylvia822@gmail.com>
2024-03-25 03:41:03 -0400

    Update ootd firestore data structure

--- backend/src/core/saveToFirestore.py
+++ backend/src/core/saveToFirestore.py
@@ -3,14 +3,33 @@ from firebase_admin import firestore
 
 def save_to_firestore(outfit_url, user_id):
     db = firestore.client()
-    ootd_collection = db.collection('OOTD')
+    # 首先，获取或创建一个以 user_id 为 ID 的文档
+    user_doc_ref = db.collection('OOTD').document(user_id)
     
-    # 创建一个新的文档s
-    doc_ref = ootd_collection.document()
-    doc_ref.set({
-        'user_id': user_id,
+    # 然后，在该文档下创建一个子集合，比如叫做 "outfits"
+    # 在 "outfits" 子集合中创建一个新的文档
+    new_outfit_ref = user_doc_ref.collection('outfits').document()
+    
+    # 设置新文档的数据
+    new_outfit_ref.set({
         'outfit_url': outfit_url,
         'created_at': firestore.SERVER_TIMESTAMP  # 使用服务器时间戳
     })
 
-    return doc_ref.id
+    return new_outfit_ref.id
+
+
+# def save_to_firestore(outfit_url, user_id):
+#     db = firestore.client()
+#     ootd_collection = db.collection('OOTD')
+    
+#     # 创建一个新的文档s
+#     doc_ref = ootd_collection.document()
+#     doc_ref.set({
+#         'user_id': user_id,
+#         'outfit_url': outfit_url,
+#         'created_at': firestore.SERVER_TIMESTAMP  # 使用服务器时间戳
+#     })
+
+#     return doc_ref.id
+
