bdfc8a527dc6307bd9eead22f730b503e179965a
PALEJADISA <67114804+PALEJADISA@users.noreply.github.com>
2024-04-20 21:37:54 -0400

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -7,8 +7,6 @@ rm -rf /opt/AppFolder/frontend/*
 cp -r /home/circleci/build/* /opt/AppFolder/frontend/
 mv -f /home/circleci/docker-compose.yml /opt/AppFolder/
 mv -f /home/circleci/nginx.conf /opt/AppFolder/
-mv -f /home/circleci/certificate.pem /opt/AppFolder/ssl/
-mv -f /home/circleci/privatekey.pem /opt/AppFolder/ssl/
 cd /opt/AppFolder
 docker-compose pull
 docker-compose up -d
